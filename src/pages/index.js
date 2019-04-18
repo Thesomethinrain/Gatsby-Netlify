e7c81bb8a47b50df7feff1e04558af4cdbd3ad37
SEB <somethinrain@gmail.com>
2019-04-18 17:16:19 +0200

    Second Change

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Salut</h1>
+    <h1>Salut !</h1>
     <p>Bienvenue sur mon nouveau site.</p>
     <p>Disponible très bientôt</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
